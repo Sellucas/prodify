711511b4773c23db5520e1d20be4abe781bf451d
Lucas Sell <lucassellmachado@gmail.com>
2024-03-25 13:58:24 -0300

    feat: add database types

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,9 +1,11 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr";
+
+import { Database } from "@/lib/types/supabase";
 
 export function supabaseClient() {
   // Create a supabase client on the browser with project's credentials
-  return createBrowserClient(
+  return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+  );
 }
--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,12 +1,14 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+import { Database } from '@/lib/types/supabase'
+
 export function supabaseServer() {
   const cookieStore = cookies()
 
   // Create a server's supabase client with newly configured cookie,
   // which could be used to maintain user's session
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
