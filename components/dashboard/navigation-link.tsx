7827e093ce2f2b102d7a84fcd1cfd23a89f2dd65
Lucas Sell <lucassellmachado@gmail.com>
2024-04-08 20:00:45 -0300

    fix: navbar link text size

--- components/dashboard/navigation-link.tsx
+++ components/dashboard/navigation-link.tsx
@@ -10,12 +10,10 @@ const NavigationLink = ({ children, name, href }: Props) => {
   return (
     <Link
       href={href}
-      className="flex p-1 rounded-xl cursor-pointer stroke-[0.75] font-semibold hover:stroke-gray-900 stroke-neutral-400 text-gray-700 hover:text-blue-600 place-items-center gap-3 hover:bg-blue-100 transition-colors duration-100"
+      className="flex p-1 rounded-xl cursor-pointer stroke-[0.75] font-medium hover:stroke-gray-900 stroke-neutral-400 text-gray-700 hover:text-blue-600 place-items-center gap-3 hover:bg-blue-100 transition-colors duration-100"
     >
       {children}
-      <p className="text-inherit font-poppins overflow-clip whitespace-nowrap tracking-wide">
-        {name}
-      </p>
+      <p className="overflow-clip whitespace-nowrap">{name}</p>
     </Link>
   );
 };
