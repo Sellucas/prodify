0919a06268d05dbfd46df158c123624f686949c8
Lucas Sell <lucassellmachado@gmail.com>
2024-04-08 20:16:13 -0300

    fix: board header padding

--- components/dashboard/board-header.tsx
+++ components/dashboard/board-header.tsx
@@ -3,10 +3,10 @@ import { CardSheet } from "@/components/dashboard/card-sheet";
 
 export const BoardHeader = () => {
   return (
-    <div className="flex justify-between bg-white mt-4 w-full h-8 py-4 lg:px-12 px-6">
-      <div className="flex items-center font-semibold">
-        <h1 className="lg:text-3xl text-2xl text-gray-700">Daily Tasks</h1>
-      </div>
+    <div className="flex justify-between items-center bg-white mt-4 w-full h-8 py-4 lg:px-12 px-4">
+      <h1 className="font-semibold text-2xl lg:text-3xl text-gray-700">
+        Daily Tasks
+      </h1>
       <div className="flex items-center gap-4">
         <CardSheet />
         <Settings className="text-gray-700 cursor-pointer" />
--- components/dashboard/board.tsx
+++ components/dashboard/board.tsx
@@ -4,7 +4,7 @@ export const Board = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <BoardHeader />
-      <div className="flex flex-row w-full h-full gap-8 overflow-scroll px-6 lg:px-12 pt-6 no-scrollbar">
+      <div className="flex flex-row w-full h-full gap-8 overflow-scroll lg:px-12 px-4 pt-6 no-scrollbar">
         {children}
       </div>
     </>
