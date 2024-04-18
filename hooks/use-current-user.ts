940c8fa13da79838650017c61007089642666353
Lucas Sell <lucassellmachado@gmail.com>
2024-04-18 16:48:26 -0300

    refactor: use shorthand for accessing 'profiles' table data

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -1,15 +1,10 @@
 import { useEffect, useState } from "react";
 
 import { supabaseClient } from "@/utils/supabase/client";
+import { Tables } from "@/lib/types/supabase";
 
 export default function useCurrentUser() {
-  const [user, setUser] = useState<{
-    created_at: string;
-    display_name: string | null;
-    email: string;
-    id: string;
-    image_url: string | null;
-  } | null>(null);
+  const [user, setUser] = useState<Tables<"profiles"> | null>(null);
 
   useEffect(() => {
     const fetchUserProfile = async () => {
