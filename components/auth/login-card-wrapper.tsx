4492dc7f078543d0dfda7e5ccc22e1cd4a0b7e04
Lucas Sell <lucassellmachado@gmail.com>
2024-06-09 13:03:01 -0300

    fix: change description position

--- components/auth/login-card-wrapper.tsx
+++ components/auth/login-card-wrapper.tsx
@@ -12,7 +12,7 @@ export const LoginCardWrapper = ({
   return (
     <div className="flex flex-col p-4 gap-4">
       <div className="space-y-8">
-        <div className="space-y-2">
+        <div className="space-y-2 text-center">
           <h1 className="text-center text-4xl font-medium leading-none tracking-tighter text-balance sm:text-5xl md:text-6xl lg:text-7xl z-10">
             {headerLabel}
           </h1>
